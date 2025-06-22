a44b40e0891f6b4bfe454c3410da0ab1e079e45d
ananthu <ananthusmk@gmail.com>
2025-06-22 22:35:22 +0530

    1

--- backend/routes/medicationRoutes.js
+++ backend/routes/medicationRoutes.js
@@ -1,13 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const auth = require("../middleware/authMiddleware");
-const { getMedications, addMedication, markTaken,deleteMedication, updateMedication } = require("../controllers/medicationController");
+const { getMedications, addMedication, markTaken } = require("../controllers/medicationController");
 
 router.use(auth);
 router.get("/", getMedications);
 router.post("/", addMedication);
 router.patch("/:id/taken", markTaken);
-router.delete("/:id", deleteMedication);
-router.put("/:id", updateMedication);
 
 module.exports = router;
